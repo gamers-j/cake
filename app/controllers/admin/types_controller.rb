055924f4387e086ab694761153ae942f5163f8c6
EC2 Default User <ec2-user@ip-172-31-41-20.ap-northeast-1.compute.internal>
2022-08-19 06:30:01 +0000

    [add]types

--- app/controllers/admin/types_controller.rb
+++ app/controllers/admin/types_controller.rb
@@ -1,14 +1,36 @@
 class Admin::TypesController < ApplicationController
   def index
+    @type = Type.new
+    @types = Type.all
   end
 
   def edit
+    @type = Type.find(params[:id])
   end
   
   def create
+    @type = Type.new(type_params)
+    if @type.save
+       redirect_to admin_types_path
+    else
+      @types = Type.all
+      render admin_types_path
+    end
   end
   
   def update
+    @type = Type.find(params[:id])
+    if @type.update(type_params)
+       redirect_to admin_types_path
+    else
+      render edit_admin_type_path
+    end
+  end
+  
+  private
+  # ストロングパラメータ
+  def type_params
+    params.require(:type).permit(:name)
   end
   
 end
