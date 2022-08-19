d27187ab0623fb1afe12afd359a84556c994534b
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-08-19 06:57:29 +0000

    [Break] public_mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,22 +1,26 @@
 class Public::CustomersController < ApplicationController
   def show
-    @customers = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-    @customers = Customer.find(params[:id])
-    unless @customers == current_user
+    @customer = Customer.find(params[:id])
+    unless @customer == current_user
     redirect_to customers_path(current_user)
     end
   end
 
   def unsubscribe
   end
-  
+
   def update
   end
-  
+
   def withdraw
   end
-  
+
+
+  def customers_params
+    params.require(:customers).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :postal_code, :telephone_namber)
+  end
 end
