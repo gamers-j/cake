030b2c22e58b5b0b1c289c5d25565a2f6c741f51
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-08-20 04:00:53 +0000

    修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,7 +6,7 @@ class Public::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
     unless @customer == current_cusotmer
-    redirect_to customers_path(current_customer)
+    redirect_to customer_path(current_customer)
     end
   end
 
