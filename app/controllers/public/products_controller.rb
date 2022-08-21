d5641d06a184e2bfcc59bb40efd0c588074acf05
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-08-21 10:57:11 +0000

    [Add]部分テンプレート・画像に関する記述の追加

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -2,6 +2,7 @@ class Public::ProductsController < ApplicationController
   def index
     @active_products = Product.where(is_active: true)
     @products = @active_products.page(params[:page]).per(8)
+    @types = Type.all
   end
 
   def show
