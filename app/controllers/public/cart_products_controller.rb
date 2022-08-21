b18b60141afe25620427123934d64c982ff990f2
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-08-21 08:16:21 +0000

    [Update] cart_products_index

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -8,17 +8,31 @@ class Public::CartProductsController < ApplicationController
     @cart_product = CartProduct.new(cart_params)
     @cart_product.customer_id = current_customer.id
     @cart_product.save
-    redirect_to cart_products_
+    redirect_to cart_products_path
   end
 
   def update
+    cart_product = CartProduct.find(params[:id])
+    cart_product.update(amount: cart_params[:amount].to_i)
+    redirect_to cart_products_path
   end
 
   def destroy
+    cart_product = CartProduct.find(params[:id])
+    cart_product.destroy
+    redirect_to cart_products_path
   end
 
   def destroy_all
+    cart_product = CartProduct.where(customer_id: current_customer.id)
+    cart_product.destroy_all
+    redirect_to products_path
   end
 
+  private
+  
+  def cart_params
+    params.require(:cart_product).permit(:product_id, :amount)
+  end
 
 end
