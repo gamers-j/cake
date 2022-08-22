f6b070cf72f8151371abdea2d7e5a8441c671490
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-08-22 03:59:41 +0000

    [Add]products_showのエラー修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ApplicationRecord
   has_many :product_orders, dependent: :destroy
   has_many :cart_produts, dependent: :destroy
 
+def  add_tax_taxed_price
+  (self.nontaxed_price * 1.10).round
+end
+
 def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
