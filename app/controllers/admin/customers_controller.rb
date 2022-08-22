adf680addb7eec5053438cbc42709e4aa2dbef8d
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-08-22 07:18:36 +0000

    [update]admin_customers_show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,12 +4,22 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(product_params)
+    redirect_to admin_product_path(@customer.id)
   end
 
+  private
+
+  def product_params
+  params.require(:product).permit(:type_id,:name,:introduction,:nontaxed_price,:is_active)
+  end
 end
