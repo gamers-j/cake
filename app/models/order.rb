3e115927ea5558220ed6afc2650f68faeddc23a9
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-08-23 06:13:50 +0000

    [Add]支払総額の修正中

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,16 @@ class Order < ApplicationRecord
   has_many :product_orders, dependent: :destroy
 
   def total_amount
+    #product_orders.taxed_price * product_orders.quantity + 800
+    total = 0
+    product_orders.eath do |product_order|
+    product_order.add_tax_taxed_price
+    
+    
+    end
+    total + 800
   end
-
+  
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, shipping_preparation: 3, sent: 4 }
 
--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -3,6 +3,9 @@ class ProductOrder < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+def  add_tax_taxed_price
+  (self.nontaxed_price * 1.10).round
+end
 
   enum making_status: { cannot_production: 0, waiting_for_production: 1, production: 2, production_completed: 3 }
 end
