08bb24ecbea89626e2a81eeb2eef23e6729823bf
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-08-23 09:15:40 +0000

    [Update] public_order_show

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,14 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :product_orders, dependent: :destroy
 
+  #商品の合計金額メソッド
+  def subtotal
+    total = 0
+    product_orders.each do |product_order|
+    total = product_order.add_tax_taxed_price * product_order.quantity
+    end
+    total
+  end
 
   def total_amount
     #product_orders.taxed_price * product_orders.quantity + 800
@@ -12,7 +20,6 @@ class Order < ApplicationRecord
     end
     total + 800
   end
-
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, shipping_preparation: 3, sent: 4 }
--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -7,5 +7,9 @@ def  add_tax_taxed_price
   (self.taxed_price * 1.10).round
 end
 
+def subtotal
+    add_tax_taxed_price * quantity
+end
+
   enum making_status: { cannot_production: 0, waiting_for_production: 1, production: 2, production_completed: 3 }
 end
