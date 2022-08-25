dd508153403fd114326ae778f9d86c47ee76cf77
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-08-25 04:45:35 +0000

    [Add]一覧画面の修正

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,6 +1,6 @@
 class Public::ProductsController < ApplicationController
   def index
-    @active_products = Product.where(is_active: true)
+    @active_products = Product.where(is_active: false)
     @products = @active_products.page(params[:page]).per(8)
     @types = Type.all
   end
