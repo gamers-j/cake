b5018fd153892626c03dbad669934bf9f3e76dba
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-08-26 06:02:56 +0000

    実験

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,17 +2,26 @@ class Admin::OrdersController < ApplicationController
    before_action :authenticate_admin!
   def show
     @order = Order.find(params[:id])
-    
+
   end
-  
+
   def update
      @order = Order.find(params[:id])
-     @order.update(order_params)
-     redirect_to admin_root_path
+     product_orders = @order.product_orders
+    if @order.update(order_params)
+      if @order.status == "入金待ち"
+        product_orders.update_all(making_status: "着手不可")
+      elsif @order.status == "入金確認"
+        product_orders.update_all(making_status: "製作待ち")
+      end
+      redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
   end
-  
+
   def order_params
     params.require(:order).permit(:status)
   end
-  
+
 end
--- app/controllers/admin/product_orders_controller.rb
+++ app/controllers/admin/product_orders_controller.rb
@@ -1,13 +1,22 @@
 class Admin::ProductOrdersController < ApplicationController
-  
+
   def update
      product_order = ProductOrder.find(params[:id])
-     product_order.update(product_order_params)
-     redirect_to admin_root_path
+     @order =  product_order.order
+     if @order.update(order_params)
+      if @order.status == "入金待ち"
+        product_orders.update_all(making_status: "着手不可")
+      elsif @order.status == "入金確認"
+        product_orders.update_all(making_status: "製作待ち")
+      end
+      redirect_to admin_order_path(@order)
+    else
+      render admin_order_path
+    end
   end
-  
+
   def product_order_params
     params.require(:product_order).permit(:making_status)
   end
-  
+
 end
